8230c7443c8095ced6f306862e3f16f91d5c53a7
BKPatt <92901369+BKPatt@users.noreply.github.com>
2022-07-04 14:47:55 -0400

    Update KingClass.cs

--- SeniorProject/Cards/KingClass/KingClass.cs
+++ SeniorProject/Cards/KingClass/KingClass.cs
@@ -1,4 +1,4 @@
-﻿using ClassesManagerReborn;
+using ClassesManagerReborn;
 using System.Collections;
 
 namespace SeniorProject.Cards.KingClass
@@ -16,13 +16,13 @@ namespace SeniorProject.Cards.KingClass
             while (!(King.Card && LilGunRandom.Card && LilMoreGunRandom.Card && MoreRandom.Card && PlayerRandom.Card && AmmoLuck.Card && HealthLuck.Card && MoreGunStats.Card && MoreMovement.Card)) yield return null;
             ClassesRegistry.Register(King.Card, CardType.Entry);
             ClassesRegistry.Register(LilGunRandom.Card, CardType.Gate, King.Card);
-            ClassesRegistry.Register(LilMoreGunRandom.Card, CardType.Card, new CardInfo[] { LilGunRandom.Card, MoreRandom.Card, PlayerRandom.Card});
+            ClassesRegistry.Register(LilMoreGunRandom.Card, CardType.Gate, King.Card);
             ClassesRegistry.Register(MoreRandom.Card, CardType.Gate, King.Card);
             ClassesRegistry.Register(PlayerRandom.Card, CardType.Gate, King.Card);
-            ClassesRegistry.Register(AmmoLuck.Card, CardType.Card, new CardInfo[] { LilGunRandom.Card, MoreRandom.Card, PlayerRandom.Card });
-            ClassesRegistry.Register(HealthLuck.Card, CardType.Card, new CardInfo[] { LilGunRandom.Card, MoreRandom.Card, PlayerRandom.Card });
-            ClassesRegistry.Register(MoreGunStats.Card, CardType.Card, new CardInfo[] { LilGunRandom.Card, MoreRandom.Card, PlayerRandom.Card });
-            ClassesRegistry.Register(MoreMovement.Card, CardType.Card, new CardInfo[] { LilGunRandom.Card, MoreRandom.Card, PlayerRandom.Card }, 2);
+            ClassesRegistry.Register(AmmoLuck.Card, CardType.Card, new CardInfo[] { LilGunRandom.Card });
+            ClassesRegistry.Register(HealthLuck.Card, CardType.Card, new CardInfo[] { MoreRandom.Card });
+            ClassesRegistry.Register(MoreGunStats.Card, CardType.Card, new CardInfo[] { LilGunRandom.Card, LilMoreGunRandom.Card });
+            ClassesRegistry.Register(MoreMovement.Card, CardType.Card, new CardInfo[] { PlayerRandom.Card }, 2);
         }
     }
 }
