1ac90f1828c62fa1a65622630a60e0c27af367ad
BKPatt <92901369+BKPatt@users.noreply.github.com>
2022-07-12 23:10:36 -0400

    Update LotteryClass.cs
    Small bug fixes and update to fit EasierExtensions update

--- SeniorProject/Cards/LotteryClass/LotteryClass.cs
+++ SeniorProject/Cards/LotteryClass/LotteryClass.cs
@@ -1,29 +1,36 @@
 using ClassesManagerReborn;
 using System.Collections;
+using SeniorProject;
 
 namespace SeniorProject.Cards.LotteryClass
 {
     class LotteryClass : ClassHandler
     {
+        private bool debug_l = false;
+
+        //Class Name
         public static string name = "Lottery";
 
         public override IEnumerator Init()
         {
             // Debugging
-            //UnityEngine.Debug.Log("Regestering: " + name);
+            if (debug_l || SeniorProject.debug_g || SeniorProject.debug_a)
+            {
+                UnityEngine.Debug.Log("Regestering: " + name);
+            }
 
             // Register class
             while (!(Lottery.Card && BetterOdds.Card && CashStrapped.Card && Chaos.Card && Fireworks.Card && LittleChaos.Card && LittleTime.Card && MoreChaos.Card && MoreTime.Card && Pyrotechnics.Card)) yield return null;
-            ClassesRegistry.Register(Lottery.Card, CardType.Entry);
-            ClassesRegistry.Register(BetterOdds.Card, CardType.Card, Lottery.Card, 4);
-            ClassesRegistry.Register(CashStrapped.Card, CardType.Card, Lottery.Card);
-            ClassesRegistry.Register(Chaos.Card, CardType.Gate, Lottery.Card);
-            ClassesRegistry.Register(Fireworks.Card, CardType.Card, new CardInfo[] { Chaos.Card }, 5);
-            ClassesRegistry.Register(LittleChaos.Card, CardType.Card, new CardInfo[] { Chaos.Card }, 5);
-            ClassesRegistry.Register(LittleTime.Card, CardType.Card, Lottery.Card);
-            ClassesRegistry.Register(MoreChaos.Card, CardType.Card, new CardInfo[] { Chaos.Card }, 2);
-            ClassesRegistry.Register(MoreTime.Card, CardType.Card, Lottery.Card, 2);
-            ClassesRegistry.Register(Pyrotechnics.Card, CardType.Card, new CardInfo[] { Chaos.Card });
+            ClassesRegistry.Register(Lottery.Card, CardType.Entry); //Lottery entry card
+            ClassesRegistry.Register(BetterOdds.Card, CardType.Card, Lottery.Card, 4); //BetterOdds Card (Max: 4)
+            ClassesRegistry.Register(CashStrapped.Card, CardType.Card, Lottery.Card); //CashStrapped Card (Max: 1)
+            ClassesRegistry.Register(Chaos.Card, CardType.Gate, Lottery.Card); //Chaos Gate Card, Unlocks Fireworks, LittleChaos, MoreChaos, and Pyrotechnics (Max: 1)
+            ClassesRegistry.Register(Fireworks.Card, CardType.Card, new CardInfo[] { Chaos.Card }, 5); //Fireworks Card, Dependent on Chaos (Max: 5)
+            ClassesRegistry.Register(LittleChaos.Card, CardType.Card, new CardInfo[] { Chaos.Card }, 5); //LittleChaos Card, Dependent on Chaos (Max: 5)
+            ClassesRegistry.Register(LittleTime.Card, CardType.Card, Lottery.Card); //LittleTime Card (Max: None)
+            ClassesRegistry.Register(MoreChaos.Card, CardType.Card, new CardInfo[] { Chaos.Card }, 2); //MoreChaos Card, Dependent on Chaos (Max: 2)
+            ClassesRegistry.Register(MoreTime.Card, CardType.Card, Lottery.Card, 2); //MoreTime Card (Max: 2)
+            ClassesRegistry.Register(Pyrotechnics.Card, CardType.Card, new CardInfo[] { Chaos.Card }); //Pyrotechnics Card, Dependent on Chaos (Max: 1)
         }
     }
 }
