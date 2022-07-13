b7990a4fec3feaff81223fe2ceace0ddd8873048
BKPatt <92901369+BKPatt@users.noreply.github.com>
2022-07-12 23:01:45 -0400

    Update GuardianClass.cs
    Small bug fixes and update to fit EasierExtensions update

--- SeniorProject/Cards/GuardianClass/GuardianClass.cs
+++ SeniorProject/Cards/GuardianClass/GuardianClass.cs
@@ -1,25 +1,32 @@
 using ClassesManagerReborn;
 using System.Collections;
+using SeniorProject;
 
 namespace SeniorProject.Cards.GuardianClass
 {
     class GuardianClass : ClassHandler
     {
+        private bool debug_l = false;
+
+        // Class Name
         public static string name = "Guardian";
 
         public override IEnumerator Init()
         {
             // Debugging
-            //UnityEngine.Debug.Log("Regestering: " + name);
+            if (debug_l || SeniorProject.debug_g || SeniorProject.debug_a)
+            {
+                UnityEngine.Debug.Log("Regestering: " + name);
+            }
 
             // Registering class
             while (!(Guardian.Card && ExtraRegen.Card && GuardiansGuardian.Card && DoubleHealth.Card && GuardiansRebirth.Card && DamageOver.Card)) yield return null;
-            ClassesRegistry.Register(Guardian.Card, CardType.Entry);
-            ClassesRegistry.Register(ExtraRegen.Card, CardType.Card, Guardian.Card, 5);
-            ClassesRegistry.Register(GuardiansGuardian.Card, CardType.Card, Guardian.Card);
-            ClassesRegistry.Register(DoubleHealth.Card, CardType.Card, Guardian.Card);
-            ClassesRegistry.Register(GuardiansRebirth.Card, CardType.Card, Guardian.Card, 3);
-            ClassesRegistry.Register(DamageOver.Card, CardType.Card, Guardian.Card);
+            ClassesRegistry.Register(Guardian.Card, CardType.Entry); //Entry class card (Max: 1)
+            ClassesRegistry.Register(ExtraRegen.Card, CardType.Card, Guardian.Card, 5); //ExtraRegen Card (Max: 5)
+            ClassesRegistry.Register(GuardiansGuardian.Card, CardType.Card, Guardian.Card); //GuardiansGuardian Card (Max: None)
+            ClassesRegistry.Register(DoubleHealth.Card, CardType.Card, Guardian.Card); //DoubleHealth Card (Max: None)
+            ClassesRegistry.Register(GuardiansRebirth.Card, CardType.Card, Guardian.Card, 3); //GuardiansRebirth Card (Max: 3)
+            ClassesRegistry.Register(DamageOver.Card, CardType.Card, Guardian.Card); //DamageOver Card (Max: None)
         }
     }
 }
