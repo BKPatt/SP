54494644d7193455c4519f0e4b57a8e79a991df2
BKPatt <92901369+BKPatt@users.noreply.github.com>
2022-07-12 23:18:41 -0400

    Update AddHealth.cs
    Small bug fixes and update to fit EasierExtensions update

--- SeniorProject/Cards/WrathClass/AddHealth.cs
+++ SeniorProject/Cards/WrathClass/AddHealth.cs
@@ -1,10 +1,9 @@
 using ClassesManagerReborn.Util;
-using System.Collections.Generic;
 using UnboundLib;
 using UnboundLib.Cards;
 using UnityEngine;
 using SeniorProject.MonoBehaviours;
-using System.Linq;
+using SeniorProject;
 
 namespace SeniorProject.Cards.WrathClass
 {
@@ -12,9 +11,11 @@ namespace SeniorProject.Cards.WrathClass
     {
         public static CardInfo Card = null;
 
+        private bool debug_l = false;
+
         public override void Callback()
         {
-            // Declares this card as part of the Pinball class
+            // Declares this card as part of the Wrath class
             gameObject.GetOrAddComponent<ClassNameMono>().className = WrathClass.name;
         }
 
@@ -23,15 +24,19 @@ namespace SeniorProject.Cards.WrathClass
             // Modifiers
             cardInfo.allowMultiple = false;
 
-            // Debug
-            //UnityEngine.Debug.Log($"[{SeniorProject.ModInitials}][Card] {GetTitle()} Built");
+            // Debugging
+            if (debug_l || SeniorProject.debug_g || SeniorProject.debug_a)
+            {
+                UnityEngine.Debug.Log($"[{SeniorProject.ModInitials}][Card] {GetTitle()} Built");
+            }
         }
 
         public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
         {
+            // Modifiers
             block.forceToAddUp += 2.0f;
 
-            // Sets new stats for Pinball class
+            // Mono(s) and adjustments
             player.gameObject.GetComponent<WrathPointAndCard>().numCards++;
             player.gameObject.GetComponent<BlockActionMono>().health = true;
             if (player.gameObject.GetComponent<BlockActionMono>().numSub == 0)
@@ -44,17 +49,26 @@ namespace SeniorProject.Cards.WrathClass
                 player.gameObject.GetComponent<BlockActionMono>().setHealth = player.gameObject.GetComponent<BlockActionMono>().numSub;
             }
 
-            // Debug
-            //UnityEngine.Debug.Log($"[{SeniorProject.ModInitials}][Card] {GetTitle()} Added to Player {player.playerID}");
+            // Debugging
+            if (debug_l || SeniorProject.debug_g || SeniorProject.debug_a)
+            {
+                UnityEngine.Debug.Log($"[{SeniorProject.ModInitials}][Card] {GetTitle()} Added to Player {player.playerID}");
+            }
         }
 
         public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
         {
-            // Sets stats back if the card is removed
+            // Remove Mono(s) and adjustments
             gameObject.GetComponent<WrathPointAndCard>().numCards--;
             gameObject.GetComponent<BlockActionMono>().health = false;
             gameObject.GetComponent<BlockActionMono>().numSub--;
             gameObject.GetComponent<BlockActionMono>().setHealth = default;
+
+            // Debugging
+            if (debug_l || SeniorProject.debug_g || SeniorProject.debug_a)
+            {
+                UnityEngine.Debug.Log($"[{SeniorProject.ModInitials}][Card] {GetTitle()} Removed from Player {player.playerID}");
+            }
         }
 
         protected override string GetTitle()
@@ -80,7 +94,7 @@ namespace SeniorProject.Cards.WrathClass
                 },
                 new CardInfoStat()
                 {
-                    positive = true,
+                    positive = false,
                     stat = "Block Force Up",
                     amount = "+2",
                     simepleAmount = CardInfoStat.SimpleAmount.notAssigned
