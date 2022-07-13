539edd5ed23ed50cb26c2b04b875c2b635044780
BKPatt <92901369+BKPatt@users.noreply.github.com>
2022-07-12 23:21:59 -0400

    Update WrathClass.cs
    Small bug fixes and update to fit EasierExtensions update

--- SeniorProject/Cards/WrathClass/WrathClass.cs
+++ SeniorProject/Cards/WrathClass/WrathClass.cs
@@ -1,27 +1,34 @@
 using ClassesManagerReborn;
 using System.Collections;
+using SeniorProject;
 
 namespace SeniorProject.Cards.WrathClass
 {
     class WrathClass : ClassHandler
     {
+        private bool debug_l = false;
+
+        //Class Name
         public static string name = "Wrath";
 
         public override IEnumerator Init()
         {
             // Debugging
-            //UnityEngine.Debug.Log("Regestering: " + name);
+            if (debug_l || SeniorProject.debug_g || SeniorProject.debug_a)
+            {
+                UnityEngine.Debug.Log("Regestering: " + name);
+            }
 
             // Register class
             while (!(Wrath.Card && FasterBlock.Card && MoreBlocks.Card && BlockAction.Card && ShockAndAwe.Card && BetterChance.Card && AddHealth.Card && AddLS.Card)) yield return null;
-            ClassesRegistry.Register(Wrath.Card, CardType.Entry);
-            ClassesRegistry.Register(FasterBlock.Card, CardType.Card, Wrath.Card);
-            ClassesRegistry.Register(MoreBlocks.Card, CardType.Card, Wrath.Card);
-            ClassesRegistry.Register(BlockAction.Card, CardType.Gate, Wrath.Card);
-            ClassesRegistry.Register(ShockAndAwe.Card, CardType.Gate, new CardInfo[] { BlockAction.Card });
-            ClassesRegistry.Register(BetterChance.Card, CardType.Card, new CardInfo[] { ShockAndAwe.Card });
-            ClassesRegistry.Register(AddHealth.Card, CardType.Card, new CardInfo[] { BlockAction.Card });
-            ClassesRegistry.Register(AddLS.Card, CardType.Card, new CardInfo[] { BlockAction.Card });
+            ClassesRegistry.Register(Wrath.Card, CardType.Entry); //Wrath entry Card
+            ClassesRegistry.Register(FasterBlock.Card, CardType.Card, Wrath.Card); //FasterBlock Card (Max: None)
+            ClassesRegistry.Register(MoreBlocks.Card, CardType.Card, Wrath.Card); //MoreBlocks Card (Max: None)
+            ClassesRegistry.Register(BlockAction.Card, CardType.Gate, Wrath.Card); //BlockAction Gate Card, Unlocks ShockAndAwe, AddHealth, and AddLs (Also BetterChance Indirectly) (Max: 1)
+            ClassesRegistry.Register(ShockAndAwe.Card, CardType.Gate, new CardInfo[] { BlockAction.Card }); //ShockAndAwe Gate Card, Dependent on Block Action and Unlocks BetterChance (Max: 1)
+            ClassesRegistry.Register(BetterChance.Card, CardType.Card, new CardInfo[] { ShockAndAwe.Card }, 5); //BetterChance Card, Dependent on ShockAndAwe (Max: 5)
+            ClassesRegistry.Register(AddHealth.Card, CardType.Card, new CardInfo[] { BlockAction.Card }); //AddHealth Card, Dependent on BlockAction (Max: 1)
+            ClassesRegistry.Register(AddLS.Card, CardType.Card, new CardInfo[] { BlockAction.Card }); //AddLS Card, Dependent on BlockAction (Max: 1)
         }
     }
 }
