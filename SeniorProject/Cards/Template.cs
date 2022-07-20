5d1d15889892c74c88e64fc43556827be3bfdf2a
BKPatt <92901369+BKPatt@users.noreply.github.com>
2022-07-20 15:16:18 -0400

    Update Template.cs
    Clean up code

--- SeniorProject/Cards/Template.cs
+++ SeniorProject/Cards/Template.cs
@@ -1,9 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnboundLib;
 using UnboundLib.Cards;
 using UnityEngine;
 
